86093ce42a91b2d2f04440736efbcf44d8b8b610
Daniel-SebastiaNC <daniel.sebastian.nc@gmail.com>
2025-03-31 20:45:45 -0300

    doc: add chapter 4.2 class

--- src/main/java/exercises/Count.java
+++ src/main/java/exercises/Count.java
@@ -1,4 +1,24 @@
+//exercises 4.2
 package exercises;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Count {
+
+    public static void main(String[] args) {
+        List<String> shoppingCart = new ArrayList<>(Arrays.asList("Banana", "Apple", "Rice", "Juice"));
+
+        System.out.println("count: " + count(shoppingCart));
+    }
+
+    private static int count(List<String> list){
+        if (list.isEmpty()) {
+            return 0;
+        }
+
+        /*list.remove(0);*/
+        return 1 + count(list.subList(1, list.size()));
+    }
 }
