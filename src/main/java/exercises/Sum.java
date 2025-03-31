ff6dcd8355c1aff4c466015f3f8fb8dae2f8904c
Daniel-SebastiaNC <daniel.sebastian.nc@gmail.com>
2025-03-31 20:45:14 -0300

    doc: add chapter 4.1 class

--- src/main/java/exercises/Sum.java
+++ src/main/java/exercises/Sum.java
@@ -1,4 +1,28 @@
+//exercises 4.1
 package exercises;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Sum {
+
+    public static void main(String[] args) {
+
+        List<Integer> array = new ArrayList<>(Arrays.asList(2, 4, 6));
+
+        System.out.println("sum: " + sum(array));
+
+    }
+
+    private static int sum(List<Integer> array){
+        if (array.isEmpty()) {
+            return 0;
+        }
+
+        /* return array.remove(0) + sum(array);*/
+        return array.get(0) + sum(array.subList(1, array.size()));
+
+    }
+
 }
